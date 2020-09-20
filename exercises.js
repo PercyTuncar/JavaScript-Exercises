5b67c2ad776e7226ac97097080579478b9312089
PercyTuncar <percy.edgar.tuncar@gmail.com>
2020-09-20 15:33:06 -0500

    Exercise getDate finished

--- exercises.js
+++ exercises.js
@@ -4,6 +4,25 @@ Today is : Tuesday.
 Current time is : 10 PM : 30 : 38
 */
 
-let today  = new Date();
-console.log(today)
+const today = new Date();
+const day = today.getDay();
+let dayList = ["Domingo","Lunes","Martes","Miercoles","JUeves","Viernes", "Sabado"]
 
+
+
+const hours = new Date();
+const hour = hours.getHours();
+const minute = hours.getMinutes();
+const second = hours.getSeconds();
+
+function format(time){
+    if(time>=13){
+        return "PM"
+    }else{
+        return "AM"
+    }
+}
+
+
+console.log("Hoy es: "+ dayList[day]);
+console.log("Current time is : "+ hour + " "+ format(hour) + " : " + minute +  " : " + second);
