841740766b7fda9ef4c45a3d1973184ee8e76384
PercyTuncar <percy.edgar.tuncar@gmail.com>
2020-09-20 14:50:51 -0500

    new Date

---
+++ exercises.js
@@ -0,0 +1,9 @@
+/**
+1. Write a JavaScript program to display the current day and time in the following format.
+Today is : Tuesday.
+Current time is : 10 PM : 30 : 38
+*/
+
+let today  = new Date();
+console.log(today)
+
